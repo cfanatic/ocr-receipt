fb0aa3078bd86bdecf6e367a16ed28fb623d2b53
Arnd <git@codefanatic.de>
2022-04-22 13:48:09 +0200

    Define detections as struct

--- include/recognition.hpp
+++ include/recognition.hpp
@@ -2,6 +2,7 @@
 #define RECOGNITION_HPP
 
 #include <iostream>
+#include <vector>
 #include <boost/filesystem.hpp>
 #include "boost/format.hpp"
 #include <leptonica/allheaders.h>
@@ -11,14 +12,29 @@ namespace ocr
 {
     class receipt
     {
+        struct detection
+        {
+            int id;
+            int x;
+            int y;
+            int w;
+            int h;
+            int conf;
+            std::string text;
+        };
+
+        friend std::ostream &operator<<(std::ostream &os, const detection &d)
+        {
+            return os << boost::format("Box[%d]: x=%d, y=%d, w=%d, h=%d, conf: %d, text: %s") % d.id % d.x % d.y % d.w % d.h % d.conf % d.text;
+        }
 
     public:
         receipt(const std::string &path);
         ~receipt();
 
         void init();
-        void extract();
-        void extract_all();
+        std::vector<detection> extract();
+        detection extract_all();
 
     private:
         std::string m_path;
--- src/recognition.cpp
+++ src/recognition.cpp
@@ -31,23 +31,28 @@ namespace ocr {
         }
     }
 
-    void receipt::extract()
+    std::vector<receipt::detection> receipt::extract()
     {
+        std::vector<receipt::detection> detections;
         Boxa *boxes = m_api->GetComponentImages(tesseract::RIL_TEXTLINE, true, true, 20, NULL, NULL, NULL);
         for (int i = 0; i < boxes->n; i++)
         {
             auto box = boxaGetBox(boxes, i, L_CLONE);
             auto &x = box->x, y = box->y, w = box->w, h = box->h;
             m_api->SetRectangle(x, y, w, h);
-            std::cout << boost::format("Box[%d]: x=%d, y=%d, w=%d, h=%d, conf: %d, text: %s") % i % x % y % w % h % m_api->MeanTextConf() % m_api->GetUTF8Text();
+            receipt::detection detection = {i, x, y, w, h, m_api->MeanTextConf(), m_api->GetUTF8Text()};
+            std::cout << detection;
+            detections.push_back(detection);
             boxDestroy(&box);
         }
-        init();
+        return detections;
     }
 
-    void receipt::extract_all()
+    receipt::detection receipt::extract_all()
     {
-        std::cout << m_api->GetUTF8Text() << std::endl;
+        receipt::detection detection = {0, 0, 0, 0, 0, m_api->MeanTextConf(), m_api->GetUTF8Text()};
+        std::cout << detection;
+        return detection;
     }
 
 } // namespace ocr
