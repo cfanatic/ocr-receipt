4618d1941aa4ecced38af85852cfcc1e0e14a43a
Arnd <git@codefanatic.de>
2022-05-27 21:39:15 +0200

    Annotate arguments and return types

--- src/receipt.py
+++ src/receipt.py
@@ -2,7 +2,7 @@ import easyocr
 
 class receipt():
 
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path
         self.reader = easyocr.Reader(["de", "en"])
         self.detections = None
@@ -14,13 +14,13 @@ class receipt():
     def preprocess(self):
         pass
 
-    def extract(self):
+    def extract(self) -> list:
         self.detections = self.reader.readtext(self.path, detail=0)
         return self.detections
 
     def overlay(self):
         pass
 
-    def process(self, detections):
+    def process(self, detections: list) -> list:
         self.articles = detections
         return self.articles
