c8875497572b7215eba11f470f468fa7fe2aebfc
Arnd <git@codefanatic.de>
2022-08-06 21:12:25 +0200

    Use OpenCV image format as input to EasyOCR

--- src/receipt.py
+++ src/receipt.py
@@ -1,21 +1,24 @@
 import easyocr
+import cv2
 
 class receipt():
 
     def __init__(self, path: str):
-        self._path = path
         self._reader = easyocr.Reader(["de", "en"])
+        self._path = path
+        self._img_cv = None
         self._detections = None
         self._articles = None
 
     def init(self):
+        self._img_cv = cv2.imread(self._path)
         pass
 
     def preprocess(self):
         pass
 
     def extract(self) -> list:
-        self._detections = self._reader.readtext(self._path, detail=0)
+        self._detections = self._reader.readtext(self._img_cv, detail=0)
         return self._detections
 
     def overlay(self):
