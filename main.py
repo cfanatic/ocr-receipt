606e7f02c61431bc3c7d37aa08c10f77f3d386de
Arnd <git@codefanatic.de>
2022-08-06 21:14:26 +0200

    Migrate the EasyOCR scripts into main.py

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 
+import easyocr
+import cv2
 import sys
 sys.dont_write_bytecode = True
 
-from src.receipt import receipt
+class receipt():
+
+    def __init__(self, path: str):
+        self._reader = easyocr.Reader(["de", "en"])
+        self._path = path
+        self._img_cv = None
+
+    def init(self):
+        self._img_cv = cv2.imread(self._path)
+        pass
+
+    def preprocess(self):
+        pass
+
+    def extract(self) -> list:
+        return self._reader.readtext(self._img_cv, detail=0)
+
+    def overlay(self):
+        pass
+
+    def process(self, detections: list) -> list:
+        articles = detections
+        return articles
 
 def main(argc, argv):
     try:
