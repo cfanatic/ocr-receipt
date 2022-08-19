0af80efe8e8eee0c8e3bf5d6aeaf8d0e71a5a4f1
Arnd <git@codefanatic.de>
2022-08-19 23:38:11 +0200

    Remove function call to detect the orientation of the input image

--- src/engine_tesseract.cpp
+++ src/engine_tesseract.cpp
@@ -24,8 +24,6 @@ namespace ocr
 
     void engine_tesseract::init()
     {
-        int orient_deg;
-        float orient_conf;
         if (m_api->Init(nullptr, "deu", tesseract::OEM_LSTM_ONLY))
         {
             std::cerr << "Error: Could not initialize Tesseract" << std::endl;
@@ -36,14 +34,6 @@ namespace ocr
             m_img_cv = cv::imread(get_path().c_str());
             m_api->SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
             m_api->SetImage(m_img_pix);
-        }
-        if (!m_api->DetectOrientationScript(&orient_deg, &orient_conf, nullptr, nullptr))
-        {
-            std::cerr << "Error: Could not detect the orientation of the input image" << std::endl;
-        }
-        else
-        {
-            std::cout << boost::format("orient_deg=%d, orient_conf=%.2f") % orient_deg % orient_conf << std::endl;
         }
     }
 
